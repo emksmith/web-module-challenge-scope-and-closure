bfcfd78e260a40973d67dbdfb14a9f84635ef60f
emksmith <116416382+emksmith@users.noreply.github.com>
2022-11-04 12:59:17 -0700

    "test"

--- index.js
+++ index.js
@@ -30,11 +30,14 @@ console.log('example task:', processFirstItem(['foo','bar'],function(str){return
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  Counter1 exists within the function, whereas counter2 is defined before the function.
   
   2. Which of the two uses a closure? How can you tell?
+  Counter1. The closure is at the return function counter() statement.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
 */
 
 // counter1 code
@@ -64,8 +67,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random()* Math.floor(3))
 }
 
 
@@ -83,10 +86,20 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*Code Here*/){
-  /*Code Here*/
-}
+function finalScore(inningsCB, innings){
+  let homeScore = 0;
+  let awayScore = 0;
+for (let i = 0; i < innings.length; i++){
+  homeScore = homeScore + inningsCB()
+  awayScore = awayScore + inningsCB()
 
+}
+return {
+  "Home": homeScore,
+  "Away": awayScore
+}
+}
+console.log('task3', finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
@@ -101,10 +114,11 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore() {
+  
+  }
 
-}
+
 
 
 /* STRETCH: ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
